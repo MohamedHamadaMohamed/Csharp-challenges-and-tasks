0571cc9232c7eed06a956b6799eef5e26f4c94aa
mohamed hamada <mohamedhamada97.07@gmail.com>
2024-10-06 19:51:45 +0300

    add task 1

---
+++ task 1 (30 -8-2024)/taks 1 p2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace taks_1_p2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 20;
+            Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+        }
+    }
+}
---
+++ task 1 (30 -8-2024)/task 1 p1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 Islam's Carpet Cleaning Service
+                 Charges:
+                        $25 per small room
+                        $35 per large room
+                Sales tax rate is 6%
+                Estimates are valid for 30 days
+             */
+            Console.Write("Number of small rooms: ");
+            int numOfSmallRooms = int.Parse(Console.ReadLine());
+
+            Console.Write("Number of large rooms: ");
+            int numOfLargeRooms = int.Parse(Console.ReadLine());
+
+            double PricePerSmallRoom =Convert.ToDouble(numOfSmallRooms*25 );
+            double PricePerLargeRoom = Convert.ToDouble(numOfLargeRooms * 35);
+            double cost = PricePerSmallRoom + PricePerLargeRoom;
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+            Console.WriteLine($"Cost: ${cost}");
+            Console.WriteLine($"Tax: ${cost*.06}");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total estimate: ${cost*1.06}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
